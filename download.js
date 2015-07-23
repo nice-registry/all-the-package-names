251ac62a406ff8b0960e547775c4e083ffcf1f9f
Zeke Sikelianos <zeke@sikelianos.com>
2015-07-23 12:30:15 -0700

    remove now-defunct download script in favor of good ol' bash

--- download.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-const fs = require('fs')
-
-require('superagent')
-  .get('https://skimdb.npmjs.com/registry/_design/scratch/_view/byField')
-  .done(function (data) {
-    fs.writeFileSync('./skimdb.json', JSON.stringify(data, null, 2))
-  })
