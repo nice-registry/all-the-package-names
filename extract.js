11b10f5bb59234edc70dc20ed2006cf46be517d5
Zeke Sikelianos <zeke@sikelianos.com>
2015-11-07 10:00:13 -0600

    use JSONStream to extract names, because the registry got too big for in-memory parsing

--- extract.js
+++ extract.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const packages = require('./skimdb.json').rows
+const JSONStream = require('JSONStream')
+const es = require('event-stream')
 const compact = require('lodash').compact
 
-var names = packages.map(function (pkg) {
-  return pkg.value.name;
-})
+var names = []
 
-fs.writeFileSync('./names.json', JSON.stringify(compact(names), null, 2))
+fs.createReadStream('./skimdb.json')
+  .pipe(JSONStream.parse('rows.*'))
+  .pipe(es.mapSync(function (pkg) {
+    names.push(pkg.value.name)
+    process.stdout.write('.')
+    return pkg
+  }))
+  .on('end', function(){
+    fs.writeFileSync('./names.json', JSON.stringify(compact(names), null, 2))
+    console.log('wrote names.json')
+  })
